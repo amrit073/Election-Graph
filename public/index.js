f68b36c8eb826c8ae5e40361e6285fd854ed02c0
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-05-16 18:00:03 +0545

    bold

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ var Kesabdatas =[];
 var Srijanadatas =[];
 
 const getData = async () => {
-    res = await fetch('https://electionupdate.herokuapp.com/api/v1/data').catch(err=>console.log(err)
-    )
+    res = await fetch('https://electionupdate.herokuapp.com/api/v1/data').catch(err=>console.log(err))
+    
     // res = await fetch('http://localhost:3000/api/v1/data')
     console.log(res);
     
@@ -19,21 +19,25 @@ const getData = async () => {
             
 }
 const fillMaps = async () => {
+     document.getElementById('gd').innerHTML = '<h1> LOADING.... </h1>'
     await getData()
 
     var BalendraShah = {
         y: Balendatas,
-        name: 'Balendra shah'
+        name: `Balendra shah - <b>${Balendatas[Balendatas.length - 1]}</b>`,
+        // text : Balendatas[Balendatas.length - 1]
     }
     var KesabSthapit = {
         y : Kesabdatas,
-        name: 'Keshab Sthapit'
+        name: `Keshab Sthapit - <b>${Kesabdatas[Kesabdatas.length - 1]}</b>`,
+        // text : Kesabdatas[Kesabdatas.length - 1]
     }
     var SrijanaShrestha ={
         y : Srijanadatas,
-        name : 'Srijana Shrestha'
+        name : `Srijana Shrestha - <b>${Srijanadatas[Srijanadatas.length - 1]}</b>`,
+        // text : Srijanadatas[Srijanadatas.length - 1]
     }
-
+     document.getElementById('gd').innerHTML = ''
     await Plotly.newPlot("gd", [BalendraShah, KesabSthapit, SrijanaShrestha]
     )
 
