ad5e6aeae67daf37f36fc25e121b0bad08c85bbe
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-05-16 14:44:51 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import cheerio from 'cheerio'
+import mongoose from 'mongoose'
+import path from 'path'
+import 'dotenv/config'
+const app = express()
+
+mongoose.connect(process.env.mongoURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}, (err) => {
+	if (err) return err
+	app.listen(process.env.PORT || 3000, () => {
+		console.log('started listenig at port ');
+	})
+})
+
+const Schema = mongoose.Schema
+
+const dataSchema = new Schema({
+	data: Object
+}, { timestamps: true })
+
+
+const myData = mongoose.model('myData', dataSchema)
+
+app.use(express.static('./public'))
+
+app.get('/api/v1/data', async (req, res) => {
+	const source = await fetch('https://election.ekantipur.com/pradesh-3/district-kathmandu/kathmandu?lng=eng')
+	const sourcetext = await source.text()
+		.catch(err => console.log(err))
+	const $ = cheerio.load(sourcetext)
+	const names = []
+	const arrv = []
+	const num = $('.list-group-flush .candidate-name')
+	const vote = $('.list-group-flush .vote-numbers')
+
+	$(num).each((i, el) => {
+		names.push(el.children[0].data)
+	})
+	$(vote).each((i, el) => {
+		arrv.push(el)
+	})
+
+	// const to_send = []
+	const mayor = []
+	for (var i = 0; i < 5; i++) {
+		mayor.push({
+			name: names[i],
+			votes: arrv[i].children[0].data
+		})
+	}
+
+	myData.find({}).exec((err, data) => {
+		// console.log(data[data.length - 1].data[0].votes==mayor[0].votes)
+		// console.log(mayor[0].votes);
+		if (data[data.length - 1].data[0].votes !== mayor[0].votes) {
+			console.log('data is not same , uploading');
+			
+			myData.create({ data: mayor }, (err, data) => {
+				if (err) throw err;
+				console.log(data);
+		
+			})
+		}
+		console.log(data);
+		
+		res.send(data)
+
+	})
+	
+		
+})
+
---
+++ public/index.js
@@ -0,0 +1,37 @@
+var Balendatas = [];
+var Kesabdatas =[];
+var Srijanadatas =[];
+
+const getData = async () => {
+    res = await fetch('http://localhost:3000/api/v1/data')
+    resj = await res.json()
+   
+    resj.forEach((e, i) => {
+        Balendatas.push(e.data[0].votes)
+        Kesabdatas.push(e.data[1].votes)
+        Srijanadatas.push(e.data[2].votes)
+    })
+            
+}
+const fillMaps = async () => {
+    await getData()
+
+    var BalendraShah = {
+        y: Balendatas,
+        name: 'Balendra shah'
+    }
+    var KesabSthapit = {
+        y : Kesabdatas,
+        name: 'Keshab Sthapit'
+    }
+    var SrijanaShrestha ={
+        y : Srijanadatas,
+        name : 'Srijana Shrestha'
+    }
+
+    await Plotly.newPlot("gd", [BalendraShah, KesabSthapit, SrijanaShrestha]
+    )
+
+}
+
+fillMaps()
