f57451e713c16c12360eb9dcd9d6a2236e9ad0c4
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-05-17 11:46:13 +0545

    image functionality

--- public/index.js
+++ public/index.js
@@ -1,15 +1,17 @@
 var Balendatas = [];
-var Kesabdatas =[];
-var Srijanadatas =[];
+var Kesabdatas = [];
+var Srijanadatas = [];
+var img_jpg = document.getElementById('jpg-export')
+
 
 const getData = async () => {
-    res = await fetch('https://electionupdate.herokuapp.com/api/v1/data').catch(err=>console.log(err))
+    res = await fetch('https://electionupdate.herokuapp.com/api/v1/data').catch(err => console.log(err))
     
     // res = await fetch('http://localhost:3000/api/v1/data')
     console.log(res);
     
     resj = await res.json()
-   console.log(resj);
+    console.log(resj);
    
     resj.forEach((e, i) => {
         Balendatas.push(e.data[0].votes)
@@ -18,8 +20,14 @@ const getData = async () => {
     })
             
 }
+const sendImg = async (options) =>{
+    fetch(`/api/v1/postimage`, options).catch(err=>{
+            console.log(err);
+        });
+}
+
 const fillMaps = async () => {
-     document.getElementById('gd').innerHTML = '<h1> LOADING.... </h1>'
+    document.getElementById('gd').innerHTML = '<h1> LOADING.... </h1>'
     await getData()
 
     var BalendraShah = {
@@ -28,19 +36,32 @@ const fillMaps = async () => {
         // text : Balendatas[Balendatas.length - 1]
     }
     var KesabSthapit = {
-        y : Kesabdatas,
+        y: Kesabdatas,
         name: `Keshab Sthapit - <b>${Kesabdatas[Kesabdatas.length - 1]}</b>`,
         // text : Kesabdatas[Kesabdatas.length - 1]
     }
-    var SrijanaShrestha ={
-        y : Srijanadatas,
-        name : `Srijana Shrestha - <b>${Srijanadatas[Srijanadatas.length - 1]}</b>`,
+    var SrijanaShrestha = {
+        y: Srijanadatas,
+        name: `Srijana Shrestha - <b>${Srijanadatas[Srijanadatas.length - 1]}</b>`,
         // text : Srijanadatas[Srijanadatas.length - 1]
     }
-     document.getElementById('gd').innerHTML = ''
-    await Plotly.newPlot("gd", [BalendraShah, KesabSthapit, SrijanaShrestha]
-    )
+    document.getElementById('gd').innerHTML = ''
+    await Plotly.newPlot("gd", [BalendraShah, KesabSthapit, SrijanaShrestha])
 
-}
+    await Plotly.toImage("gd", { format: "jpeg", height: 500, width: 700 }).then((url) => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({url})
+        }
+        console.log(options);
+        sendImg(options)
+        
+        
+    })
+
+} 
 
 fillMaps()
